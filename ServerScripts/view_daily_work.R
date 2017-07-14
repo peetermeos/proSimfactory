9e351df9581f2e7b15012316a9e5612c38a77684
Peeter Meos <peeterm@proekspert.ee>
2017-07-14 11:42:22 +0300

    Bugfix

--- ServerScripts/view_daily_work.R
+++ ServerScripts/view_daily_work.R
@@ -28,45 +28,45 @@ serviceCode <- function(){
   printLog("Init")
   printLog("Loading started, getting last 24 hrs")
   
-  #load("c:/Temp/2017-01-18.RData")
-  #df <- df.ods
+  load("c:/Temp/2017-01-18.RData")
+  df <- df.ods
   
-  t2 <- as.POSIXct(Sys.Date()) 
-  t1 <- t2 - 3600 * 24 
-  
-  t1 <- format(t1, "%Y-%m-%d")
-  t2 <- format(t2, "%Y-%m-%d")
-  
-  sql <- list()
-  sql$db.name <- "SAPMEWIP"
-  sql$host.name <- "eeel163.encnet.ead.ems"
-  sql$driver.name <- "SQL Server"
-  sql$port <- ""
-  sql$user.name <- "proekspert"
-  sql$pwd <- "proekspert1!"
-  
-  s.odbc <- paste("DRIVER=", sql$driver.name,
-                  ";Database=", sql$db.name,
-                  ";Server=", sql$host.name,
-                  ";Port=", sql$port,
-                  ";PROTOCOL=TCPIP",
-                  ";UID=", sql$user.name,
-                  ";PWD=", sql$pwd,
-                  ";TDS_Version=8.0", sep = "")  
-  db <- odbcDriverConnect(s.odbc)
-  
-  df <- sqlQuery(db, paste("SELECT SFC, ACTION_CODE, DATE_TIME, SFC, OPERATION, ITEM, ITEM_REVISION, 
-                            ROUTER, ROUTER_REVISION, STEP_ID, RESRCE, SHOP_ORDER_BO, PARTITION_DATE
-                            FROM dbo.ACTIVITY_LOG
-                            WHERE DATE_TIME >='", t1, "' AND DATE_TIME   <= '", t2, "'", sep = ""))
-  
-  odbcClose(db)
-  if (nrow(df) > 2) 
-    printLog("Loading successful")
-  else{
-    printLog("Loading failed")
-    stop()
-  }
+  # t2 <- as.POSIXct(Sys.Date()) 
+  # t1 <- t2 - 3600 * 24 
+  # 
+  # t1 <- format(t1, "%Y-%m-%d")
+  # t2 <- format(t2, "%Y-%m-%d")
+  # 
+  # sql <- list()
+  # sql$db.name <- "SAPMEWIP"
+  # sql$host.name <- "eeel163.encnet.ead.ems"
+  # sql$driver.name <- "SQL Server"
+  # sql$port <- ""
+  # sql$user.name <- "proekspert"
+  # sql$pwd <- "proekspert1!"
+  # 
+  # s.odbc <- paste("DRIVER=", sql$driver.name,
+  #                 ";Database=", sql$db.name,
+  #                 ";Server=", sql$host.name,
+  #                 ";Port=", sql$port,
+  #                 ";PROTOCOL=TCPIP",
+  #                 ";UID=", sql$user.name,
+  #                 ";PWD=", sql$pwd,
+  #                 ";TDS_Version=8.0", sep = "")  
+  # db <- odbcDriverConnect(s.odbc)
+  # 
+  # df <- sqlQuery(db, paste("SELECT SFC, ACTION_CODE, DATE_TIME, SFC, OPERATION, ITEM, ITEM_REVISION, 
+  #                           ROUTER, ROUTER_REVISION, STEP_ID, RESRCE, SHOP_ORDER_BO, PARTITION_DATE
+  #                           FROM dbo.ACTIVITY_LOG
+  #                           WHERE DATE_TIME >='", t1, "' AND DATE_TIME   <= '", t2, "'", sep = ""))
+  # 
+  # odbcClose(db)
+  # if (nrow(df) > 2) 
+  #   printLog("Loading successful")
+  # else{
+  #   printLog("Loading failed")
+  #   stop()
+  # }
 
   df <- df[!is.na(df$RESRCE),]
   df <- df[!is.na(df$OPERATION),]
@@ -117,6 +117,7 @@ serviceCode <- function(){
                    sep =  "");
 
   printLog("Returning dataset")
+  write.table(plotStr, file="text.txt")
   s <- toJSON(list(result = data.frame(), plot = plotStr))
   return(s)
 }
--- ServerScripts/view_failed_items.R
+++ ServerScripts/view_failed_items.R
@@ -43,45 +43,45 @@ serviceCode <- function(){
   printLog("Init")
   printLog("Loading started, getting last 24 hrs")
   
-  load("c:/Temp/2017-01-18.RData")
-  df <- df.ods
+  #load("c:/Temp/2017-01-18.RData")
+  #df <- df.ods
   
-  # t2 <- as.POSIXct(Sys.Date())
-  # t1 <- t2 - 3600 * 24
-  # 
-  # t1 <- format(t1, "%Y-%m-%d")
-  # t2 <- format(t2, "%Y-%m-%d")
-  # 
-  # sql <- list()
-  # sql$db.name <- "SAPMEWIP"
-  # sql$host.name <- "eeel163.encnet.ead.ems"
-  # sql$driver.name <- "SQL Server"
-  # sql$port <- ""
-  # sql$user.name <- "proekspert"
-  # sql$pwd <- "proekspert1!"
-  # 
-  # s.odbc <- paste("DRIVER=", sql$driver.name,
-  #                 ";Database=", sql$db.name,
-  #                 ";Server=", sql$host.name,
-  #                 ";Port=", sql$port,
-  #                 ";PROTOCOL=TCPIP",
-  #                 ";UID=", sql$user.name,
-  #                 ";PWD=", sql$pwd,
-  #                 ";TDS_Version=8.0", sep = "")
-  # db <- odbcDriverConnect(s.odbc)
-  # 
-  # df <- sqlQuery(db, paste("SELECT SFC, ACTION_CODE, DATE_TIME, SFC, OPERATION, ITEM, ITEM_REVISION,
-  #                           ROUTER, ROUTER_REVISION, STEP_ID, RESRCE, SHOP_ORDER_BO, PARTITION_DATE
-  #                           FROM dbo.ACTIVITY_LOG
-  #                           WHERE DATE_TIME >='", t1, "' AND DATE_TIME   <= '", t2, "'", sep = ""))
-  # 
-  # odbcClose(db)
-  # if (nrow(df) > 2)
-  #   printLog("Loading successful")
-  # else{
-  #   printLog("Loading failed")
-  #   stop()
-  # }
+  t2 <- as.POSIXct(Sys.Date())
+  t1 <- t2 - 3600 * 24
+
+  t1 <- format(t1, "%Y-%m-%d")
+  t2 <- format(t2, "%Y-%m-%d")
+
+  sql <- list()
+  sql$db.name <- "SAPMEWIP"
+  sql$host.name <- "eeel163.encnet.ead.ems"
+  sql$driver.name <- "SQL Server"
+  sql$port <- ""
+  sql$user.name <- "proekspert"
+  sql$pwd <- "proekspert1!"
+
+  s.odbc <- paste("DRIVER=", sql$driver.name,
+                  ";Database=", sql$db.name,
+                  ";Server=", sql$host.name,
+                  ";Port=", sql$port,
+                  ";PROTOCOL=TCPIP",
+                  ";UID=", sql$user.name,
+                  ";PWD=", sql$pwd,
+                  ";TDS_Version=8.0", sep = "")
+  db <- odbcDriverConnect(s.odbc)
+
+  df <- sqlQuery(db, paste("SELECT SFC, ACTION_CODE, DATE_TIME, SFC, OPERATION, ITEM, ITEM_REVISION,
+                            ROUTER, ROUTER_REVISION, STEP_ID, RESRCE, SHOP_ORDER_BO, PARTITION_DATE
+                            FROM dbo.ACTIVITY_LOG
+                            WHERE DATE_TIME >='", t1, "' AND DATE_TIME   <= '", t2, "'", sep = ""))
+
+  odbcClose(db)
+  if (nrow(df) > 2)
+    printLog("Loading successful")
+  else{
+    printLog("Loading failed")
+    stop()
+  }
 
   ###### Data Analysis ######
   
@@ -136,7 +136,7 @@ serviceCode <- function(){
   write.csv(df.ret, file = "data.csv")
 
   # Prepare data for plotting
-  df$hour <- format(as.POSIXct(df$event_end), format = "%d %H")
+  df$hour <- format(as.POSIXct(df$event_end), format = "%d %b %H:00 ")
   df$count <- 1
   
   df <- aggregate(data = df, count ~ RESRCE  + hour, FUN = "sum", na.rm = TRUE)
@@ -144,7 +144,7 @@ serviceCode <- function(){
   
   lr <- levels(df$RESRCE)
   lh <- levels(df$hour)
-    
+   
   ##### Creating a plot ######
   printLog("Creating the plot")
   c <- "["
