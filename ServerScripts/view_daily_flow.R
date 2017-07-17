017d8f365f307aa2f7883331071d35b9c4bc7361
Peeter Meos <peeterm@proekspert.ee>
2017-07-17 14:57:57 +0300

    Item duration view added.

--- ServerScripts/view_daily_flow.R
+++ ServerScripts/view_daily_flow.R
@@ -1,8 +1,8 @@
 source("ServerScripts/utils.R")
 
 metadata <- list(
-  title <- "DailyComponentFlow",
-  version = "v0.0.2",
+  title = "DailyComponentFlow",
+  version = "v0.0.3",
   description <- "SFC workflow for past 24 hrs",
   inputs = list(),
   outputs = list(result = "character")
@@ -29,11 +29,11 @@ serviceCode <- function(){
   # load("c:/Temp/2017-01-18.RData")
   # df <- df.ods
   
-  t2 <- as.POSIXct(Sys.Date())
+  t2 <- as.POSIXct(Sys.time())
   t1 <- t2 - 3600 * 24
 
-  t1 <- format(t1, "%Y-%m-%d")
-  t2 <- format(t2, "%Y-%m-%d")
+  t1 <- format(t1, "%Y-%m-%d %H:%M:%S")
+  t2 <- format(t2, "%Y-%m-%d %H:%M:%S")
 
   sql <- list()
   sql$db.name <- "SAPMEWIP"
